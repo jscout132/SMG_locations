e95d8fc5548a5ecd23c18478024167163df96dd0
Jacob Nelson <scoutnelson132@gmail.com>
2024-02-29 14:28:07 -0500

    updating JS script call

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-from config import arc_gis_api, map_id
+#from config import arc_gis_api, map_id
 
 app = Flask(__name__)
 app.debug = False
 
 @app.route('/')
 def home():
-    return render_template('index.html', arc_gis_api=arc_gis_api, map_id=map_id)
+    return render_template('index.html')#, arc_gis_api=arc_gis_api, map_id=map_id)
 
